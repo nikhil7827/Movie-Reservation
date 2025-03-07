52d57fdc4c77edd20528085027b08f445edb57b0
nikhil7827 <ndharmajigar@gmail.com>
2025-03-07 08:44:10 -0600

    add

--- app/models/movie.py
+++ app/models/movie.py
@@ -3,7 +3,7 @@ from app import db
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), nullable=False, unique=True)
     description = db.Column(db.Text, nullable=False)
     genre = db.Column(db.String(50), nullable=False)
     poster_url = db.Column(db.String(200), nullable=True, default='https://via.placeholder.com/150')
